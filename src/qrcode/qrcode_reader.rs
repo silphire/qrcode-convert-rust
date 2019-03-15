ec78c0a5ab1eb9fcc53aa1462e8898bae3997314
Yuki Mitsui <silphire@gmail.com>
2019-03-16 08:09:57 +0900

    added module_size()

--- src/qrcode/qrcode_reader.rs
+++ src/qrcode/qrcode_reader.rs
@@ -25,6 +25,12 @@ impl<'a> Reader<'a> for QRCodeReader {
 impl QRCodeReader {
     pub fn extract_pure_bits(&self, image: &BitMatrix) -> BitMatrix {
         let left_top_black = image.get_top_left_on_bit();
+        let right_bottom_black = image.get_bottom_right_on_bit();
+        if left_top_black.len() == 0 || right_bottom_black.len() == 0 {
+            // throw NotFound
+        }
+
+        let module_size = self.module_size(&left_top_black, image);
 
         // TODO implement
         return BitMatrix {
@@ -33,5 +39,30 @@ impl QRCodeReader {
             height: image.height,
             row_size: image.row_size,
         };
+    }
+
+    fn module_size(&mut, left_top_black: &Vec<usize>, image: BitMatrix) -> f64 {
+        let x = left_top_black[0];
+        let y = left_top_black[1];
+        let in_black = true;
+        let transition = 0;
+
+        while x < image.width && y < image.heght {
+            if in_black != image.get(x, y) {
+                transition += 1;
+                if transition == 5 {
+                    break;
+                }
+                in_black = !in_black;
+            }
+            x += 1;
+            y += 1;
+        }
+
+        if x == width || y == height {
+            // throw not found
+        }
+
+        return (x - left_top_black[0]) / 7.0;
     }
 }
