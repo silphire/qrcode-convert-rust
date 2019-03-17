4859e2c6ec2133fb947d84cf22bbfdc05fd721a0
Yuki Mitsui <silphire@gmail.com>
2019-03-17 10:48:11 +0900

    added DecoderResult

---
+++ src/common/decoder_result.rs
@@ -0,0 +1,18 @@
+pub struct DecoderResult {
+    raw_bytes: Vec<u8>, 
+    num_bits: usize,
+    text: str,
+    byte_segments: Vec<Vec<u8>>,
+    ec_level: str,
+    errors_corrected: isize,
+    erasures: isize,
+    // other,
+    structured_append_parity: isize,
+    structured_append_sequence_number: isize,
+}
+
+impl DecoderResult {
+    fn has_structured_append(&self) {
+        return self.structured_append_parity >= 0 && self.structured_append_sequence_number >= 0;
+    }
+}
