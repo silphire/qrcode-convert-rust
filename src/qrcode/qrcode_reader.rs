9d61a1378a6597aa71b915223a24294c71a00b0c
Yuki Mitsui <silphire@gmail.com>
2019-03-19 07:20:25 +0900

    added extra_pure_bits

--- src/qrcode/qrcode_reader.rs
+++ src/qrcode/qrcode_reader.rs
@@ -12,7 +12,7 @@ pub struct QRCodeReader {
 impl<'a> Reader<'a> for QRCodeReader {
     fn decode(&mut self, image: &BinaryBitmap, hints: Option<&HashMap<DecodeHintType, u8>>) {
         if hints.map_or(false, |v| (*v).contains_key(&DecodeHintType::PureBarcode)) {
-            let bits: BitMatrix = self.extract_pure_bits(image.get_black_matrix());
+            let bits: BitMatrix = QRCodeReader::extract_pure_bits(image.get_black_matrix());
         } else {
         }
     }
@@ -23,14 +23,14 @@ impl<'a> Reader<'a> for QRCodeReader {
 }
 
 impl QRCodeReader {
-    pub fn extract_pure_bits(&self, image: &BitMatrix) -> BitMatrix {
+    pub fn extract_pure_bits(image: &BitMatrix) -> BitMatrix {
         let left_top_black = image.get_top_left_on_bit();
         let right_bottom_black = image.get_bottom_right_on_bit();
         if left_top_black.len() == 0 || right_bottom_black.len() == 0 {
             // throw NotFound
         }
 
-        let module_size = self.module_size(&left_top_black, image);
+        let module_size = QRCodeReader::module_size(&left_top_black, image);
 
         // TODO implement
         return BitMatrix {
@@ -41,13 +41,13 @@ impl QRCodeReader {
         };
     }
 
-    fn module_size(&mut, left_top_black: &Vec<usize>, image: BitMatrix) -> f64 {
-        let x = left_top_black[0];
-        let y = left_top_black[1];
-        let in_black = true;
-        let transition = 0;
+    fn module_size(left_top_black: &Vec<usize>, image: &BitMatrix) -> f64 {
+        let mut x = left_top_black[0];
+        let mut y = left_top_black[1];
+        let mut in_black = true;
+        let mut transition = 0;
 
-        while x < image.width && y < image.heght {
+        while x < image.width && y < image.height {
             if in_black != image.get(x, y) {
                 transition += 1;
                 if transition == 5 {
@@ -59,10 +59,10 @@ impl QRCodeReader {
             y += 1;
         }
 
-        if x == width || y == height {
+        if x == image.width || y == image.height {
             // throw not found
         }
 
-        return (x - left_top_black[0]) / 7.0;
+        return (x - left_top_black[0]) as f64 / 7.0;
     }
 }
