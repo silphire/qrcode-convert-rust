b761b564d24197d08b892b5bb552454cd4c59aef
Yuki Mitsui <silphire@gmail.com>
2019-03-28 06:43:19 +0900

    implementing WhiteRectangleDetector

--- src/common/detector/white_rectangle_detector.rs
+++ src/common/detector/white_rectangle_detector.rs
@@ -1,5 +1,6 @@
 use crate::common::bitmatrix::BitMatrix;
 use crate::result_point::ResultPoint;
+use crate::common::detector::math_utils;
 
 pub struct WhiteRectangleDetector {
     image: BitMatrix,
@@ -10,6 +11,9 @@ pub struct WhiteRectangleDetector {
     down_init: usize,
     up_init: usize,
 }
+
+const INIT_SIZE: usize = 10;
+const CORR: f64 = 1.0;
 
 impl WhiteRectangleDetector {
     pub fn new(image: BitMatrix, init_size: usize, x: usize, y: usize) -> Option<WhiteRectangleDetector> {
@@ -73,6 +77,47 @@ impl WhiteRectangleDetector {
         return &vec![];
     }
 
+    fn get_black_point_on_segment(&self, ax: f64, ay: f64, bx: f64, by: f64) -> Option<ResultPoint> {
+        let dist = math_utils::round(math_utils::distance(ax, ay, bx, by));
+        let x_step = (bx - ax) / dist as f64;
+        let y_step = (by - ay) / dist as f64;
+        for i in 0..dist {
+            let x = math_utils::round(ax + (i as f64) * x_step) as usize;
+            let y = math_utils::round(ay + (i as f64) * y_step) as usize;
+            if self.image.get(x, y) {
+                return Some(ResultPoint {x: x as f64, y: y as f64});
+            }
+        }
+        return None;
+    }
+
+    fn center_edges(&self, y: &ResultPoint, z: &ResultPoint, x: &ResultPoint, t: &ResultPoint) -> Vec<ResultPoint> {
+        let yi = y.x;
+        let yj = y.y;
+        let zi = z.x;
+        let zj = z.y;
+        let xi = x.x;
+        let xj = x.y;
+        let ti = t.x;
+        let tj = t.y;
+
+        if yi < (self.width / 2) as f64 {
+            return vec![
+                ResultPoint{x: ti - CORR, y: tj + CORR},
+                ResultPoint{x: zi + CORR, y: zj + CORR},
+                ResultPoint{x: xi - CORR, y: xj - CORR},
+                ResultPoint{x: yi + CORR, y: yj - CORR},
+            ];
+        } else {
+            return vec![
+                ResultPoint{x: ti + CORR, y: tj + CORR},
+                ResultPoint{x: zi + CORR, y: zj - CORR},
+                ResultPoint{x: xi - CORR, y: xj + CORR},
+                ResultPoint{x: yi - CORR, y: yj - CORR},
+            ];
+        }
+    }
+
     fn contains_black_point(&self, a: usize, b: usize, fixed: usize, horizontal: bool) -> bool {
         if horizontal {
             for x in a..(b + 1) {
@@ -87,7 +132,7 @@ impl WhiteRectangleDetector {
                 }
             }
         }
-        
+
         return false;
     }
 }
