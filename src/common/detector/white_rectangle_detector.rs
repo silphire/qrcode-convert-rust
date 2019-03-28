e65365296fce91ef0adf27b379ca30ace8da0f3d
Yuki Mitsui <silphire@gmail.com>
2019-03-29 05:50:20 +0900

    implemented WhiteRectangleDetector

--- src/common/detector/white_rectangle_detector.rs
+++ src/common/detector/white_rectangle_detector.rs
@@ -72,9 +72,131 @@ impl WhiteRectangleDetector {
                 size_exceeded = true;
                 break;
             }
+
+            let bottom_border_not_white = true;
+            while (bottom_border_not_white || !at_least_one_black_point_found_on_right) && down < self.height {
+                bottom_border_not_white = self.contains_black_point(left, right, down, true);
+                if bottom_border_not_white {
+                    down += 1;
+                    a_black_point_found_on_border = true;
+                    at_least_one_black_point_found_on_bottom = true;
+                } else if !at_least_one_black_point_found_on_bottom {
+                    down += 1;
+                }
+            }
+
+            if down >= self.height {
+                size_exceeded = true;
+                break;
+            }
+
+            let left_border_not_white = true;
+            while (left_border_not_white || !at_least_one_black_point_found_on_right) && left >= 0 {
+                left_border_not_white = self.contains_black_point(up, down, left, true);
+                if left_border_not_white {
+                    left -= 1;
+                    a_black_point_found_on_border = true;
+                    at_least_one_black_point_found_on_left = true;
+                } else if !at_least_one_black_point_found_on_left {
+                    left -= 1;
+                }
+            }
+
+            if left < 0 {   // TODO for usize
+                size_exceeded = true;
+                break;
+            }
+
+            let top_border_not_white = true;
+            while (top_border_not_white || !at_least_one_black_point_found_on_top) && up >= 0 {
+                top_border_not_white = self.contains_black_point(left, right, up, true);
+                if top_border_not_white {
+                    up -= 1;
+                    a_black_point_found_on_border = true;
+                    at_least_one_black_point_found_on_top = true;
+                } else if !at_least_one_black_point_found_on_top {
+                    up -= 1;
+                }
+            }
+
+            if up < 0 {   // TODO for usize
+                size_exceeded = true;
+                break;
+            }
         }
 
-        return &vec![];
+        if size_exceeded {
+            return &vec![];
+        }
+
+        let max_size = right - left;
+        let i: usize;
+
+        let mut z: Option<ResultPoint> = None;
+        for i in 1..max_size {
+            z = self.get_black_point_on_segment(
+                left as f64,
+                (down - i) as f64,
+                (left + i) as f64,
+                down as f64);
+            if z.is_some() {
+                break;
+            }
+        }
+
+        if z.is_none() {
+            return &vec![]; // TODO Err?
+        }
+
+        let mut t : Option<ResultPoint> = None;
+        for i in 1..max_size {
+            t = self.get_black_point_on_segment(
+                left as f64,
+                (up + i) as f64,
+                (left + i) as f64,
+                up as f64);
+            if t.is_some() {
+                break;
+            }
+        }
+
+        if t.is_none() {
+            return &vec![]; // TODO Err?
+        }
+
+        let mut x : Option<ResultPoint> = None;
+        for i in 1..max_size {
+            x = self.get_black_point_on_segment(
+                right as f64,
+                (up + i) as f64,
+                (right - i) as f64,
+                up as f64);
+            if x.is_some() {
+                break;
+            }
+        }
+
+        if x.is_none() {
+            return &vec![]; // TODO Err?
+        }
+
+        let mut y : Option<ResultPoint> = None;
+        for i in 1..max_size {
+            y = self.get_black_point_on_segment(
+                right as f64,
+                (down - i) as f64,
+                (right - i) as f64,
+                down as f64);
+            if y.is_some() {
+                break;
+            }
+        }
+
+        if y.is_none() {
+            return &vec![]; // TODO Err?
+        }
+
+        return self.center_edges(&y.unwrap(), &z.unwrap(), &x.unwrap(), &t.unwrap());
     }
 
     fn get_black_point_on_segment(&self, ax: f64, ay: f64, bx: f64, by: f64) -> Option<ResultPoint> {
@@ -91,7 +213,7 @@ impl WhiteRectangleDetector {
         return None;
     }
 
-    fn center_edges(&self, y: &ResultPoint, z: &ResultPoint, x: &ResultPoint, t: &ResultPoint) -> Vec<ResultPoint> {
+    fn center_edges(&self, y: &ResultPoint, z: &ResultPoint, x: &ResultPoint, t: &ResultPoint) -> &Vec<ResultPoint> {
         let yi = y.x;
         let yj = y.y;
         let zi = z.x;
@@ -102,14 +224,14 @@ impl WhiteRectangleDetector {
         let tj = t.y;
 
         if yi < (self.width / 2) as f64 {
-            return vec![
+            return &vec![
                 ResultPoint{x: ti - CORR, y: tj + CORR},
                 ResultPoint{x: zi + CORR, y: zj + CORR},
                 ResultPoint{x: xi - CORR, y: xj - CORR},
                 ResultPoint{x: yi + CORR, y: yj - CORR},
             ];
         } else {
-            return vec![
+            return &vec![
                 ResultPoint{x: ti + CORR, y: tj + CORR},
                 ResultPoint{x: zi + CORR, y: zj - CORR},
                 ResultPoint{x: xi - CORR, y: xj + CORR},
