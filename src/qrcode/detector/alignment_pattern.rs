5d14c30a6267edb8569b9bd75d91534d89a34b5a
Yuki Mitsui <silphire@gmail.com>
2019-04-01 05:24:09 +0900

    added AlignmentPattern

--- src/qrcode/detector/alignment_pattern.rs
+++ src/qrcode/detector/alignment_pattern.rs
@@ -6,11 +6,27 @@ pub struct AlignmentPattern {
     pub estimated_module_size: f64,
 }
 
-impl ResultPointTrait for AlignmentPattern {}
+impl ResultPointTrait for AlignmentPattern {
+    fn get_x(&self) -> f64 {
+        return self.x;
+    }
+
+    fn set_x(&mut self, x: f64) {
+        self.x = x;
+    }
+
+    fn get_y(&self) -> f64 {
+        return self.y;
+    }
+
+    fn set_y(&mut self, y: f64) {
+        self.y = y;
+    }
+}
 
 impl AlignmentPattern {
     pub fn about_equals(&self, module_size: f64, i: f64, j: f64) -> bool {
-        if (i - self.y).abs() <= module_size && (j - self.x) <= module_size {
+        if (i - self.get_y()).abs() <= module_size && (j - self.get_x()) <= module_size {
             let module_size_diff = (module_size - self.estimated_module_size).abs();
             return module_size_diff <= 1.0 || module_size_diff <= self.estimated_module_size;
         }
@@ -19,8 +35,8 @@ impl AlignmentPattern {
     }
 
     pub fn combine_estimate(&self, i: f64, j: f64, new_module_size: f64) -> AlignmentPattern {
-        let combined_x = (self.x + j) / 2.0;
-        let combined_y = (self.y + i) / 2.0;
+        let combined_x = (self.get_x() + j) / 2.0;
+        let combined_y = (self.get_y() + i) / 2.0;
         let combined_module_size = (self.estimated_module_size + new_module_size) / 2.0;
 
         return AlignmentPattern {
