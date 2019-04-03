735d81fa7291ca6882e55121c73072129a5831f5
Yuki Mitsui <silphire@gmail.com>
2019-04-04 00:43:02 +0900

    added finder_pattern

--- src/qrcode/detector/finder_pattern.rs
+++ src/qrcode/detector/finder_pattern.rs
@@ -1,8 +1,11 @@
 use crate::result_point::ResultPointTrait;
+use std::num;
 
 pub struct FinderPattern {
     x: f64, 
     y: f64,
+    estimated_module_size: f64,
+    count: isize,
 }
 
 impl ResultPointTrait for FinderPattern {
@@ -20,5 +23,40 @@ impl ResultPointTrait for FinderPattern {
 
     fn set_y(&mut self, y: f64) {
         self.y = y;
+    }
+}
+
+impl FinderPattern {
+    fn new(pos_x: f64, pos_y: f64, estimated_module_size: f64, count: isize) -> FinderPattern {
+        return FinderPattern {
+            x: pos_x,
+            y: pos_y,
+            estimated_module_size: estimated_module_size,
+            count: count,
+        }
+    }
+
+    pub const fn get_estimated_module_size(&self) -> f64 {
+        return self.estimated_module_size;
+    }
+
+    pub const fn get_count(&self) -> isize {
+        return self.count;
+    }
+
+    fn about_equals(&self, module_size: f64, i: f64, j: f64) -> bool {
+        if (i - self.get_y()).abs() <= module_size && (j - self.get_x()).abs() <= module_size {
+            let module_size_diff = (module_size - self.get_estimated_module_size()).abs();
+            return module_size_diff <= 1.0 || module_size_diff <= self.get_estimated_module_size();
+        }
+        return false;
+    }
+
+    fn combine_estimate(&self, i: f64, j: f64, new_module_size: f64) -> FinderPattern {
+        let combined_count = self.get_count() + 1;
+        let combined_x = (self.get_count() as f64 * self.get_x() + j) / combined_count as f64;
+        let combined_y = (self.get_count() as f64 * self.get_y() + i) / combined_count as f64;
+        let combined_module_size = (self.get_count() as f64 * self.get_estimated_module_size() + new_module_size) / combined_count as f64;
+        return FinderPattern::new(combined_x, combined_y, combined_module_size, combined_count);
     }
 }
