6ebe9274e63e2cce321ebf988f5e5b33b390eb84
Yuki Mitsui <silphire@gmail.com>
2019-04-05 00:19:44 +0900

    implemented found_pattern_cross()

--- src/qrcode/detector/finder_pattern_finder.rs
+++ src/qrcode/detector/finder_pattern_finder.rs
@@ -1,3 +1,4 @@
+use std::any::Any;
 use std::collections::HashMap;
 use crate::common::bitmatrix::BitMatrix;
 use crate::decode_hint_type::DecodeHintType;
@@ -7,7 +8,7 @@ use crate::qrcode::detector::finder_pattern_info::FinderPatternInfo;
 pub struct FinderPatternFinder {
     image: BitMatrix,
     possible_centers: Vec<isize>,
-    cross_check_state_count: Vec<isize>,  // TODO fixed size
+    cross_check_state_count: [isize; 5],
     result_point_callback: fn(point: &ResultPoint),
 }
 
@@ -16,7 +17,11 @@ const MIN_SKIP: isize = 3;
 const MAX_MODULES: isize = 97;
 
 impl FinderPatternFinder {
-    fn find_with_hints(&self, hints: Option<HashMap<DecodeHintType, i32>>) -> FinderPatternInfo {
+    pub fn new(image: BitMatrix, result_point_callback: &fn(point: &ResultPoint)) -> FinderPatternFinder {
+        unimplemented!();
+    }
+
+    pub fn find_with_hints(&self, hints: Option<HashMap<DecodeHintType, &Any>>) -> FinderPatternInfo {
         let try_harder = hints.map_or(false, |v| v.contains_key(&DecodeHintType::TryHarder));
         let max_i = self.image.height;
         let max_j = self.image.width;
@@ -34,5 +39,29 @@ impl FinderPatternFinder {
 
     const fn center_from_end(&self, state_count: &[isize], end: isize) -> f64 {
         return (end - state_count[4] - state_count[3]) as f64 - state_count[2] as f64 / 2.0;
+    }
+
+    fn found_pattern_cross(state_count: [isize; 5]) -> bool {
+        let total_module_size = 0;
+        for i in 0..5 {
+            let count = state_count[i];
+            if count == 0 {
+                return false;
+            }
+            total_module_size += count;
+        }
+        if total_module_size < 7 {
+            return false;
+        }
+
+        let module_size = total_module_size as f64 / 7.0;
+        let max_variance = module_size / 2.0;
+
+        return 
+            (module_size - state_count[0] as f64).abs() < max_variance && 
+            (module_size - state_count[1] as f64).abs() < max_variance && 
+            (3.0 * module_size - state_count[2] as f64).abs() < 3.0 * max_variance && 
+            (module_size - state_count[3] as f64).abs() < max_variance && 
+            (module_size - state_count[4] as f64).abs() < max_variance; 
     }
 }
