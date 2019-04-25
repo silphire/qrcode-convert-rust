07338549375cf048e55d05748b930b8844c10244
Yuki Mitsui <silphire@gmail.com>
2019-04-26 08:43:26 +0900

    added error_correction_level

--- src/qrcode/decoder/data_block.rs
+++ src/qrcode/decoder/data_block.rs
@@ -1,9 +1,21 @@
+use crate::qrcode::decoder::error_correction_level::ErrorCorrectionLevel;
+use crate::qrcode::decoder::version::Version;
+
 pub struct DataBlock {
-    num_data_codewards: isize,
+    num_data_codewords: isize,
+    codewords: [u8],
 }
 
 impl DataBlock {
-    pub const fn get_num_data_codewards(&self) -> isize {
-        return self.num_data_codewards;
+    pub fn get_data_blocks(raw_codewords: &[u8], version: &Version, ec_level: &ErrorCorrectionLevel) -> Vec<DataBlock> {
+        unimplemented!();
+    }
+    
+    pub const fn get_num_data_codewords(&self) -> isize {
+        return self.num_data_codewords;
+    }
+
+    pub const fn get_codewords(&self) -> &[u8] {
+        return &self.codewords;
     }
 }
---
+++ src/qrcode/decoder/error_correction_level.rs
@@ -0,0 +1,6 @@
+pub enum ErrorCorrectionLevel {
+    L = 0x01,
+    M = 0x00,
+    Q = 0x03,
+    H = 0x02,
+}
