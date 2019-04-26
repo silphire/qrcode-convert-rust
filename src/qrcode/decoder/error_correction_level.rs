d48d268cde6d84b0483f3b37eca1bf77bd55915c
Yuki Mitsui <silphire@gmail.com>
2019-04-27 06:22:22 +0900

    added implementation of error_correction_level

--- src/qrcode/decoder/error_correction_level.rs
+++ src/qrcode/decoder/error_correction_level.rs
@@ -4,3 +4,19 @@ pub enum ErrorCorrectionLevel {
     Q = 0x03,
     H = 0x02,
 }
+
+const FOR_BITS: [ErrorCorrectionLevel; 4] = [
+    ErrorCorrectionLevel::M,
+    ErrorCorrectionLevel::L,
+    ErrorCorrectionLevel::H,
+    ErrorCorrectionLevel::Q,
+];
+
+impl ErrorCorrectionLevel {
+    pub fn for_bits(bits: usize) -> ErrorCorrectionLevel {
+        if bits < 0 && bits >= FOR_BITS.len() {
+            unimplemented!();
+        }
+        return FOR_BITS[bits];
+    }
+}
