e6dd6a67450aa01c4061b9d1828c8fd807a55ec5
Yuki Mitsui <silphire@gmail.com>
2019-05-06 04:53:34 +0900

    added get_data_blocks

--- src/qrcode/decoder/data_block.rs
+++ src/qrcode/decoder/data_block.rs
@@ -13,7 +13,64 @@ impl DataBlock {
             return Err(Error::IllegalArgumentError);
         }
 
-        unimplemented!();
+        let ec_blocks = version.get_ec_blocks_for_level(ec_level);
+
+        let total_blocks = 0;
+        let ec_block_array = ec_blocks.get_ec_blocks();
+        for ec_block in ec_block_array {
+            total_blocks += ec_block.get_count();
+        }
+
+        let result = vec![];
+        let num_result_blocks = 0;
+        for ec_block in ec_block_array {
+            for i in 0..ec_block.get_count() {
+                let num_data_codewords = ec_block.get_data_codewords();
+                let num_block_codewords = ec_blocks.get_ec_codewords_per_block() + num_data_codewords;
+                let data_block = DataBlock {
+                    num_data_codewords: num_data_codewords, 
+                    codewords: vec![0; num_block_codewords as usize],
+                };
+                result.push(data_block);
+                num_result_blocks += 1;
+            }
+        }
+
+        let shoter_blocks_total_codewords = result[0].codewords.len();
+        let longer_blocks_start_at = result.len() - 1;
+        while longer_blocks_start_at >= 0 {
+            let num_codewords = result[longer_blocks_start_at].codewords.len();
+            if num_codewords == shoter_blocks_total_codewords {
+                break;
+            }
+            longer_blocks_start_at -= 1;
+        }
+        longer_blocks_start_at -= 1;
+
+        let shorter_blocks_num_data_codewords = shoter_blocks_total_codewords - ec_blocks.get_ec_codewords_per_block() as usize;
+        let raw_codewords_offset = 0;
+        for i in 0..shorter_blocks_num_data_codewords {
+            for j in 0..num_result_blocks {
+                result[j].codewords[i] = raw_codewords[raw_codewords_offset];
+                raw_codewords_offset += 1;
+            }
+        }
+
+        for j in longer_blocks_start_at..num_result_blocks {
+            result[j].codewords[shorter_blocks_num_data_codewords] = raw_codewords[raw_codewords_offset];
+            raw_codewords_offset += 1;
+        }
+
+        let max = result[0].codewords.len();
+        for i in shorter_blocks_num_data_codewords..max {
+            for j in 0..num_result_blocks {
+                let i_offset = if j < longer_blocks_start_at { i } else { i + 1};
+                result[j].codewords[i_offset] = raw_codewords[raw_codewords_offset];
+                raw_codewords_offset += 1;
+            }
+        }
+
+        return Ok(result);
     }
     
     pub const fn get_num_data_codewords(&self) -> isize {
